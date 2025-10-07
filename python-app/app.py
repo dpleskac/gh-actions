2dbdc264b7814f22e53d943b295a23a77ad29c1b
Daniel Pleskac <pleskac@cisco.com>
2025-10-07 08:15:47 +0200

    feat: build python app workflow

---
+++ python-app/app.py
@@ -0,0 +1,6 @@
+# app.py
+# This is a simple Python script to print a greeting.
+
+if __name__ == "__main__":
+    print("Hello from the Dockerized Python application!")
+    print("This image was built by GitHub Actions!")
